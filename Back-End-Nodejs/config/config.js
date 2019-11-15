e0612fea1a8cc8da494b08f45a1dbc0bee32b158
YoniMelako <44365680+YoniMelako@users.noreply.github.com>
2019-11-15 03:01:55 +0200

    Update config.js

--- Back-End-Nodejs/config/config.js
+++ Back-End-Nodejs/config/config.js
@@ -1,6 +1,6 @@
 let mongoUrl =
-  "mongodb+srv://yonimelako:0502006336@zerotelcome-y5yhi.mongodb.net/test?retryWrites=true&w=majority";
-let secretKey = "301630315z";
+  "";
+let secretKey = "";
 
 module.exports = {
   mongoUrl: mongoUrl,
